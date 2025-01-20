a53b1f64a7e996e74be87898d718acf0032ff7cd
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-01-20 18:55:17 -0300

    refactor: update layout and styling, remove unused SVGs, and add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="flex flex-row items-center min-h-screen bg-[#FAF2E1]">
+      <div className="w-[70%]">a</div>
+      <div className="bg-[]">b</div>
+    </div>
+  );
+}
