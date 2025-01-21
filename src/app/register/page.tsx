58ea9f30030c171d2536e7e71fcde433f81b2c71
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-01-21 20:18:25 -0300

    fix: refine layout for login and registration pages by fix borders

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Register() {
   return (
     <div className="flex flex-row items-center min-h-screen justify-between bg-bg w-screen">
-      <div className="w-[70vw] min-h-screen border-r-[1px] border-r-[#CFBB90] flex justify-center items-center">
+      <div className="w-[70vw] min-h-screen flex justify-center items-center">
         <Image src="/logo02.png" alt="logo02" width={500} height={500} />
       </div>
-      <div className="w-[30vw] min-h-screen bg-fbg py-6 px-16 flex flex-col">
+      <div className="w-[30vw] min-h-screen border-l-[1px] border-l-[#CFBB90] bg-fbg py-6 px-16 flex flex-col">
         <div className="flex items-center justify-center text-orange-pizza text-[3rem]">
           <div className="-mr-[0.7rem]">Dona</div>
           <Image src="/pizza02.png" alt="pizza02" width={106} height={97} />
