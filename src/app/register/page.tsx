4391a97bd985dc2688b72069f7e25e1cdb6d1d79
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-01-22 19:10:08 -0300

    feat: refactor login and registration pages to use AuthTitle component for consistency

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,6 @@
 import InfoCard from "@/components/auth/info-card";
 import RegisterForm from "@/components/auth/register-form";
+import AuthTitle from "@/components/auth/title";
 import { UtensilsCrossed } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,11 +12,7 @@ export default function Register() {
         <Image src="/logo02.png" alt="logo02" width={500} height={500} />
       </div>
       <div className="w-[30vw] min-h-screen border-l-[1px] border-l-[#CFBB90] bg-fbg py-6 px-16 flex flex-col">
-        <div className="flex items-center justify-center text-orange-pizza text-[3rem]">
-          <div className="-mr-[0.7rem]">Dona</div>
-          <Image src="/pizza02.png" alt="pizza02" width={106} height={97} />
-          <div>Vilma</div>
-        </div>
+        <AuthTitle />
         <div className="text-[2rem] my-2 text-orange-pizza flex flex-col">
           <span>Crie sua</span>
           <span>Conta</span>
