4e9e2df0f1edfc853adf78c20dc352d60e0faa14
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-01-23 18:16:18 -0300

    fix: enhance dropdown menu styling for improved user experience

--- src/components/nav/menu/nav-menu.tsx
+++ src/components/nav/menu/nav-menu.tsx
@@ -17,23 +17,32 @@ export default function NavMenu() {
           <Menu className="h-8 w-8" />
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40">
+      <DropdownMenuContent className="w-40 bg-bg border-2 border-border-pizza ">
         <DropdownMenuLabel className="text-center">Menu</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator className="bg-border-pizza" />
         <div className="space-y-2 p-1">
-          <DropdownMenuGroup asChild className="flex items-center gap-x-1">
+          <DropdownMenuGroup
+            asChild
+            className="flex items-center gap-x-1 hover:bg-fbg rounded-md"
+          >
             <Link href={"#"}>
               <House className="h-5 w-5" />
               <span>Inicio</span>
             </Link>
           </DropdownMenuGroup>
-          <DropdownMenuGroup asChild className="flex items-center gap-x-1">
+          <DropdownMenuGroup
+            asChild
+            className="flex items-center gap-x-1 hover:bg-fbg rounded-md"
+          >
             <Link href={"#promocoes"}>
               <PiggyBank className="h-5 w-5" />
               <span>Promoções</span>
             </Link>
           </DropdownMenuGroup>
-          <DropdownMenuGroup asChild className="flex items-center gap-x-1">
+          <DropdownMenuGroup
+            asChild
+            className="flex items-center gap-x-1 hover:bg-fbg rounded-md"
+          >
             <Link href={"#mais-vendidas"}>
               <Flame className="h-5 w-5" />
               <span>Mais Vendidas</span>
