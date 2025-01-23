6c3255b632073a4cd9748c02ab7ab2dbb9c2bf8b
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-01-23 15:57:49 -0300

    fix: enhance responsive design for login and register pages; adjust AuthTitle styling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,11 +10,11 @@ export default function Register() {
     <div className="flex flex-row items-center min-h-screen justify-between bg-bg w-screen">
       <Link
         href="/"
-        className="w-[70vw] min-h-screen flex justify-center items-center"
+        className="md:w-[70vw] min-h-screen justify-center items-center md:flex hidden"
       >
         <Image src="/logo02.png" alt="logo02" width={500} height={500} />
       </Link>
-      <div className="w-[30vw] min-h-screen border-l-[1px] border-l-[#CFBB90] bg-fbg py-6 px-16 flex flex-col">
+      <div className="w-full md:w-[30vw] min-h-screen border-l-[1px] border-l-[#CFBB90] bg-fbg py-6 px-8 md:px-16 flex flex-col">
         <AuthTitle />
         <div className="text-[2rem] my-2 text-orange-pizza flex flex-col">
           <span>Crie sua</span>
--- src/components/auth/title.tsx
+++ src/components/auth/title.tsx
@@ -5,7 +5,7 @@ export default function AuthTitle() {
   return (
     <Link
       href={"/"}
-      className="flex items-center justify-center text-orange-pizza text-[3rem] font-bold italic"
+      className="flex items-center justify-center text-orange-pizza text-[2rem] sm:text-[2.5rem] lg:text-[2.5rem] xl:text-[2.5rem] 2xl:text-[3rem]  font-bold italic"
     >
       <div className="-mr-[0.7rem]">Dona</div>
       <Image src="/pizza02.png" alt="pizza02" width={106} height={97} />
